ff085d3ba43542378b6f51e18f8fb4b14ea12af8
unknown <austin8897@gmail.com>
2018-11-10 21:02:09 -0600

    THIS commit is good

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
     $.ajax({
         type: "POST",
-        url: "http://api.openweathermap.org/data/2.5/weather?id=" + $("#citySelect").val() + "&appid=API-KEY&units=metric",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=0c3738d3ccdcae43ef79b3e1f49656a1&units=metric",
         dataType: "json",
         success: function (result, status, xhr) {
-            var table = $("<table><tr><th>Weather Description</th></tr>");
+            var table = $("<table><tr><th>Chicago</th></tr>");
 
-            table.append("<tr><td>City:</td><td>" + result["name"] + "</td></tr>");
-            table.append("<tr><td>Country:</td><td>" + result["sys"]["country"] + "</td></tr>");
             table.append("<tr><td>Current Temperature:</td><td>" + result["main"]["temp"] + "°C</td></tr>");
             table.append("<tr><td>Humidity:</td><td>" + result["main"]["humidity"] + "</td></tr>");
             table.append("<tr><td>Weather:</td><td>" + result["weather"][0]["description"] + "</td></tr>");
